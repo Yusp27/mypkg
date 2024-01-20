b8fd90cf0e82f91dbc6a5a244b4f440363c8913f
Yuta Ogura <yusp110732@gmail.com>
2024-01-20 17:48:03 +0900

    fix

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -6,15 +6,19 @@ class Talker():
     def _init_(self, node):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
-        node.create_timer(0.5, cb)
+        node.create_timer(0.5, self.cb)
 
-def cb():
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
-rclpy.init()
-node = Node("talker")
-talker = Talker(node)
-rclpy.spin(node)
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
